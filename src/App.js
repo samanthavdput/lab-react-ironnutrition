0c7f4e9d01012826495f245f01d8e5f0c8cd1181
Samantha van der Put <samantha.vdput@gmail.com>
2021-01-23 17:34:32 +0100

    iteration 4 done

--- src/App.js
+++ src/App.js
@@ -4,27 +4,43 @@ import 'bulma/css/bulma.css';
 import foods from './foods.json';
 import FoodBox from './components/FoodBox'
 import AddFood from './components/AddFood'
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
   state = {
-    foods
+    foods: foods,
+    displayedFoods: [...foods]
   }
 
   handleAddFood = (newFood) => {
     const newFoods = [newFood, ...this.state.foods]
-    console.log('help')
-    this.setState({foods: newFoods})
+    this.setState({
+      foods: newFoods,
+      displayedFoods: newFoods
+    })
+  }
+
+  handleFoodSearch = (searchString) => {
+    const newFoods = this.state.foods.filter(food => (food.name.toUpperCase().includes(searchString.toUpperCase())))
+    this.setState({
+      displayedFoods: newFoods
+    })
   }
 
   render() {
-    const foodList = this.state.foods.map(element => {
+    const foodList = this.state.displayedFoods.map(element => {
       return <FoodBox key={element.name} food={element} />
 
     })
     return (
       <div className="App">
+
+      <SearchBar filterFood={this.handleFoodSearch} />
+
+      {foodList}
+
         <AddFood addFood={this.handleAddFood} />
-        {foodList}
+
       </div>
     );
   }
