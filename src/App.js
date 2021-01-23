83cfb82c7ecb9e7c1c7fa5cae8a6bdaedf024bde
Dirk Kelderman <dirkkelderman@gmail.com>
2021-01-23 14:22:07 +0100

    itartion 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
+import FoodBox from './components/FoodBox'
+import AddFood from './components/AddFood'
 
 function App() {
+
+  const foodList = foods.map(element => {
+    return <FoodBox key={element.name} food={element} />
+    
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddFood />
+      {foodList}
     </div>
   );
 }
---
+++ src/components/AddFood.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+ class AddFood extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            name: '',
+            calories: 0,
+            image: ''
+        }
+    }
+
+    handleChange = (event) => {
+
+        const propertyName = event.target.name
+
+        this.setState({
+            [propertyName]: event.target.value
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <label>Name:</label>
+                        <input type='text' name='name' value={this.state.name} onChange={this.handleChange} ></input>
+
+                    <label>Calories:</label>
+                        <input type='text' name='calories' value={this.state.calories} onChange={this.handleChange}></input>
+
+                    <label>Image:</label>
+                        <input type='text' name='image' value={this.state.image} onChange={this.handleChange}></input>
+
+                    <button>Submit</button>
+
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddFood
