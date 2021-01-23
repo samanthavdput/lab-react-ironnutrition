a34a6a8f69d0a2126162fd620aa4bd47fdd88474
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-01-23 14:29:48 +0100

    resolve conflict

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
-<<<<<<< HEAD
 import FoodBox from './components/FoodBox'
 import AddFood from './components/AddFood'
 
@@ -11,24 +10,13 @@ function App() {
   const foodList = foods.map(element => {
     return <FoodBox key={element.name} food={element} />
     
-=======
-import FoodBox from "./components/FoodBox.js"
-
-function App() {
-
-  const foodList = foods.map( element => {
-    return <FoodBox key={element.name} food={element} />
->>>>>>> 13edab0bd3f1db8f6765b5a9fd21539869d2912c
   })
 
   return (
     <div className="App">
-<<<<<<< HEAD
+      <h1>test</h1>
       <AddFood />
       {foodList}
-=======
-      { foodList }
->>>>>>> 13edab0bd3f1db8f6765b5a9fd21539869d2912c
     </div>
   );
 }
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
 function FoodBox(props) {
     return (
       <div className="box">
@@ -31,40 +30,6 @@ function FoodBox(props) {
         </article>
       </div>
     );
-=======
-const FoodBox = (props) => {
-    return (
-    <div className="box">
-        <article className="media">
-            <div className="media-left">
-            <figure className="image is-64x64">
-                <img src={props.food.image} alt={props.food.name} />
-            </figure>
-            </div>
-            <div className="media-content">
-            <div className="content">
-                <p>
-                <strong>{props.food.name}</strong> <br />
-                <small>{props.food.calories}</small>
-                </p>
-            </div>
-            </div>
-            <div className="media-right">
-            <div className="field has-addons">
-                <div className="control">
-                <input className="input" type="number" value="1" />
-                </div>
-                <div className="control">
-                <button className="button is-info">
-                    +
-                </button>
-                </div>
-            </div>
-            </div>
-        </article>
-    </div>
-    )
->>>>>>> 13edab0bd3f1db8f6765b5a9fd21539869d2912c
 }
 
 export default FoodBox
